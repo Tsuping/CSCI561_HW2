8fa83f0c69f77300e0f1c7da90706299b4109383
Tsuping <32005572+Tsuping@users.noreply.github.com>
2022-02-23 22:36:23 -0800

    delete dead tiles

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
 
+from numpy import delete
+
 
 input = "input.txt"
 
@@ -19,7 +21,6 @@ def read_input(file):
     return player, prev_board, current_board
 
 player, prev_board, current_board = read_input(input)
-print(player)
 
 def write_output(output_file, move):
     with open(output_file, 'w') as F:
@@ -30,6 +31,7 @@ def delete_tile(board, dead_piece_list):
         board[piece[0]][piece[1]] = 0
     return board
 
+
 def ko_rule(current_board, prev_board):
     for i in range(5):
         for j in range(5):
@@ -38,47 +40,99 @@ def ko_rule(current_board, prev_board):
 
     return True
 
+def find_liberty(board, row, col):
+    count = 0
+    for point in find_ally_cluster(board, row, col):
+        neighboring = find_neighbor(board, point[0], point[1])
+        for check in neighboring:
+            if board[check[0]][check[1]] == 0:
+                count += 1
+    return count
+
 def find_dead_tile(board, player):
     dead_tile = list()
     for i in range(5):
         for j in range(5):
             if board[i][j] == player:
-                if find_liberty(board, i, j) == 0 and (i,j) not in dead_tile:
+                if not find_liberty(board, i, j) and (i,j) not in dead_tile:
                     dead_tile.append((i,j))
     return dead_tile
 
+
+
 def find_neighbor(board, i, j):
     neighbor = []
+    board = delete_dead_tiles(board, (i, j))
     if i > 0:
         neighbor.append((i-1,j))
-    if i < 5:
-        neighbor.append((i + 1,j))
+    if i < 4:
+        neighbor.append((i+1,j))
     if j > 0:
-        neighbor.append((i, j - 1))
-    if j < 5:
-        neighbor.append((i, j + 1))
+        neighbor.append((i, j-1))
+    if j < 4:
+        neighbor.append((i, j+1))
     return neighbor
 
 
 def find_neighbor_ally(board, i, j):
     allies = []
     for stones in find_neighbor(board, i, j):
-        print(stones)
         if board[i][j] == board[stones[0]][stones[1]]:
             allies.append(stones)
     return allies
 
 
+def find_ally_cluster(board, i, j):
+    ally_cluster = list()
+    queue = []
+    queue.append((i, j))
+    while queue:
+        node = queue.pop()
+        ally_cluster.append((node[0], node[1]))
+        for neighbor in find_neighbor_ally(board, node[0], node[1]):
+            if neighbor not in queue and neighbor not in ally_cluster:
+                queue.append(neighbor)
+    return ally_cluster
 
-def find_liberty(board, row, col):
-    count = 0
-    #Need one more loop(function) to check the adjacent ally tile and everything below this will be include in this loop
-    neighboring = find_neighbor(board, row, col)
-    for check in neighboring:
-        if board[check[0]][check[1]] == 0:
-            count += 1
-    return count
+def delete_dead_tiles(board, player):
+    check = find_dead_tile(board, player)
+    if len(check) == 0:
+        return board
+    new_board = delete_tile(board, check)
+    return new_board
 
-print(find_neighbor_ally(current_board, 0, 2))
+
+
+file1 = open("output_maze.txt", "w")
+
+
+for i in range(len(current_board)):
+    string = []
+    for word in current_board[i]:
+        string.append(str(word))
+    string2 = " ".join(string)
+    file1.write(string2 + "\n")
+
+
+after_delete = delete_dead_tiles(current_board, int(player))
+print(after_delete)
+
+
+
+file1.write("after delete" + "\n")
+for i in range(len(after_delete)):
+    string = []
+    for word in after_delete[i]:
+        string.append(str(word))
+    string2 = " ".join(string)
+    file1.write(string2 + "\n")
+
+    
+
+
+
+
+
+
 
 
