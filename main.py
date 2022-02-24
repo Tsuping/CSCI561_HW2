1646662f00627c1424eb1bb5166aa89e8dc484d0
Tsuping <32005572+Tsuping@users.noreply.github.com>
2022-02-23 16:45:16 -0800

    Done with KO_rule

--- main.py
+++ main.py
@@ -13,11 +13,9 @@ def read_input(file):
     prev_board = []
     current_board = []
     for i in range(1, Board_size + 1):
-        for value in info[i]:
-            prev_board.append(int(value))
+        prev_board.append([int(value) for value in info[i]])
     for i in range(Board_size + 1, len(info)):
-        for value in info[i]:
-            current_board.append(int(value))
+        current_board.append([int(value) for value in info[i]])
     return player, prev_board, current_board
 
 player, prev_board, current_board = read_input(input)
@@ -25,5 +23,18 @@ player, prev_board, current_board = read_input(input)
 def write_output(output_file, move):
     with open(output_file, 'w') as F:
         F.write(move)
+
+def delete_tile(board, dead_piece_list):
+    for piece in dead_piece_list:
+        board[piece[0]][piece[1]] = 0
+    return board
+
+def ko_rule(current_board, prev_board):
+    for i in range(5):
+        for j in range(5):
+            if current_board[i][j] != prev_board[i][j]:
+                return False
+
+    return True
 
 
